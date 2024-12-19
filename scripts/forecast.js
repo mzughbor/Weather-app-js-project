ddee292052c06f34c25c03400a9aa5ae9fc6a5c3
mahmoud zughbor <mzughbor@gmail.com>
2024-12-19 19:59:44 +0200

    first commit

---
+++ scripts/forecast.js
@@ -0,0 +1,23 @@
+const key = "D2dl2G9TZZtSwE0xJQeDkYXAdhbMTJSF";
+
+// Get the weather information's from the API
+const getWeather = async (id) => {
+  const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
+
+// Get the city information's from the API
+const getCity = async (city) => {
+  const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
